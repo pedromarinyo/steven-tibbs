ced62ddce4b2b596c31128da649b338b222f9c9e
jeanhochu-home <jeanhochu@gmail.com>
2014-10-21 00:09:21 -0400

    Items added
    Added items

--- scripts/items.js
+++ scripts/items.js
@@ -16,4 +16,30 @@
                             Ferst,            //item's current locaton;
         );
         
+        
+        var Poision = new Item(
+                            "Poison",            //item's name
+                            Ferst,            //item's current locaton;
+        );
+        
+        
+        var Bat = new Item(
+                            "Bat",            //item's name
+                           Clough,            //item's current locaton;
+        );
+        
+        
+         var Injection = new Item(
+                            "Injection",            //item's name
+                           Skiles,            //item's current locaton;
+        );
+        
+        
+         var Injection = new Item(
+                            "Rope",            //item's name
+                           Student Center,            //item's current locaton;
+        );
+        
+        
+        
     } window.initLoc = function initLoc();
