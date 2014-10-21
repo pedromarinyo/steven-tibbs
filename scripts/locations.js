88798c6a6fae5a1b10efd3c9412ec9d4f0896a4a
jeanhochu-home <jeanhochu@gmail.com>
2014-10-21 00:12:25 -0400

    Added Locations
    Added Locations

--- scripts/locations.js
+++ scripts/locations.js
@@ -16,4 +16,49 @@
                             {90.839273, 896.82938}                  //locaton's coordinates
         );
         
+        
+         var Library = new Loc(
+                            "Library",            //name
+                            {90.839273, 896.82938}                  //locaton's coordinates
+        );
+        
+        
+         var Fountain = new Loc(
+                            "Fountain",            //name
+                            {90.839273, 896.82938}                  //locaton's coordinates
+        );
+        
+        
+         var StudentCenter = new Loc(
+                            "Student Center",            //name
+                            {90.839273, 896.82938}                  //locaton's coordinates
+        );
+        
+        
+         var Clough = new Loc(
+                            "Clough",            //name
+                            {90.839273, 896.82938}                  //locaton's coordinates
+        );
+        
+        
+         var VanLeer = new Loc(
+                            "VanLeer",            //name
+                            {90.839273, 896.82938}                  //locaton's coordinates
+        );
+        
+        
+         var Skiles = new Loc(
+                            "Skiles",            //name
+                            {90.839273, 896.82938}                  //locaton's coordinates
+        );
+        
+        
+         var HealthCenter = new Loc(
+                            "HealthCenter",            //name
+                            {90.839273, 896.82938}                  //locaton's coordinates
+        );
+        
+    
+        
+        
     } window.initLoc = function initLoc();
