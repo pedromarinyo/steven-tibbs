342eaaac49f96144a865ba01700a2c60a4562aa6
Lianghao <chenlh@gatech.edu>
2014-11-19 11:47:58 -0500

    Add skeleton for quest generation

--- scripts/quest.js
+++ scripts/quest.js
@@ -13,24 +13,76 @@ car
 */
 //class of tools used for killing
 (function (window) {
-  function tool(name, evidence   ) {
-        this.name     = name;
-        this.evidence=evidence;
+	function questGenerator(tool, killer, crimescene) {
+		this.tool = tool;
+		this.killer = killer;
+		this.crimescene = crimescene;
+
+        this.evidenceAccomplished = new Array();
+        this.allEvidences = new Array();
+        
+        // Generate all possible evidences
+        this.allEvidences.push("Evidence: " + this.tool);
+        
         
         this.questAccomplished = new Array();
-        this.questTried = new Array();
-
-       
+        this.allQuests = new Array();
+        
+        // Generate all possible quests
+        var name = "find the " + this.tool;
+        var description = "Go to " + this.crimescene + " to find " + this.tool;
+        var destination = this.crimescene;
+        var evidenceRequired = new Array();
+        var award = "Evidence: " + this.tool;
+        this.allQuests.push(new quest(name, description, destination, evidenceRequired, award));
+        
+        
+        this.nextQuests = function nextQuests() {
+        	var quests = new Array();
+        	for (var i = 0; i < this.allQuests.length; i++) {
+        		if (this.questAccomplished.indexOf(this.allQuests[i]) == -1) {
+        			var hasEvidenceRequired = true;
+        			for (var j = 0; j < this.allQuests[i].evidenceRequired.length; j++) {
+        				if (this.evidenceAccomplished.indexOf(this.allQuests[i].evidenceRequired[j]) == -1) {
+        					hasEvidenceRequired = false;
+        				}
+        			}
+        			if (hasEvidenceRequired) {
+        				quests.push(this.allQuests[i]);
+        			}
+        		}
+        	}
+        	return quests;
         }
         
+        this.finshQuest = function finshQuest(quest) {
+        	this.questAccomplished.push(quest);
+        	this.evidenceAccomplished.push(quest.award);
+        }
+	}
+	
+// 	function tool(name, evidence) {
+//         this.name     = name;
+//         this.evidence=evidence;
+//         
+//         this.questAccomplished = new Array();
+//         this.questTried = new Array();   
+//     }   
+//         
+// 	function inspecttools(name, clue) {
+//         this.name     = name;
+//     	this.clue	  = clue;
+// 	}
+  
+	function quest(name, description, destination, evidenceRequired, award){
+		this.name = name;
+		this.description = description;
+		this.destination = destination;
+		this.evidenceRequired = evidenceRequired;
+		this.award = award;
+	}
         
-        
- function inspecttools(name, clue) {
-        this.name     = name;
-       
-  }
-        
-  }(window));   //but i dont understand what this window stuff is doing. do i need it to make a class? 
+}(window));   //but i dont understand what this window stuff is doing. do i need it to make a class? 
        
         
         
