b9342795a7c9d38da1cf0efcb514febcdfc44430
jeanhochu-home <jeanhochu@gmail.com>
2014-11-19 09:03:41 -0500

    started programming quests.

--- scripts/quest.js
+++ scripts/quest.js
@@ -1,161 +1,4 @@
 /*
-Quest Generation
-This is the general step to find out. There are at least two or three clues and two witness per each item. 
-One witness is to tell where the clue is, and the other witness is who was close to tibbs and who can give more information about personal clues. 
-At the end, there will be one helper, who is close to steve tibbs, to tell who was an enemy, or why that person was the enemy. 
-
-1. Find clue #1 of the method for killing
-	1.1. Find clue from the scene (From the Ferst Center) or from the hospital 
-	1.2. Got clue for the method
-	1.3. Got some clue for who killed 
-
-2. Study the clue #1
-	2.1. Go to another location to investigate the killer based on the clue #1
-	2.2. Got partial clue for the killer 
-	2.3. But some clues are missing missing. 
-	 	 
-3. Find a witness #1 or a consultant and find clue #2
-	3.1. Go to the location of the witness
-	3.2. Ask questions to the witness  
-	3.3. Got  clue #2 location
-	
-4. Study clue #2 
-	4.1. Go to the location to meet with professionals.
-	4.2. Investigate clue #2 to get more information about the suspect
-	4.3. But some clues are still missing
-	4.4. Find where you can find another clue 
-	
-5. Find a witness #2 
-	5.1. Go to the location to meet with the witness who was close to steve tibbs 
-	5.2. Learned about clue #3  
-
-6. Study clue #3 
-	6.1. Find clue #3
-	6.2. Investigate clue #3  
-	6.3. Found out who is the killer, and why he killed. 
-	
- 
-
-
-<GUN>
-If the character used the Gun, the clues would be
-(Body-Bullet, Fingerprint from the gun, and a threat voice mail sent earlier to Steve Tibbs)  
-
-Finding Bullet
-1. Find a gun hole from Tibb's body. 
-2. Find the bullet and use it as a clue
-3. Track the bullet
-4. Narrowed down to few people
-
-Finding the Fingerprint
-5. Find witness who heard or saw the gun
-6. Find the abandoned gun from the grass
-7. Find finger prints from the gun 
-8. Narrowed down to fewer people 
-
-Finding the Voicemail
-9. From the gun, Learned that the suspect left a threatening voice mail earlier to Tibbs
-10. Listening to the voicemail, it is clear who  killed, and why killed.
-
-
-
-<POISON>
-If the character used the Poison, the clues would be
-( Fingerprint from the wineglass,  memo about the poison, and Steve Tibb's diary )  
-
-Finding wineglass
-1. Find a broken wineglass.
-2. Find the fingerprint from the wineglass and use it as a clue
-3. Narrowed down to few people
-
-Finding the memo about the poison from a left bag  
-5. Find an unattended bag from the bus stop
-6. Find the extra poison and memo about the poison 
-7. Narrowed down to fewer people by tracking the writing style and books checked out. 
-
-Finding the Diary
-8. Found out that Steve Tibb's romantic relationship( or collaborative relationship) ended with bitterness
-9. Become enemy to kill Tibbs. 
-
-
-
-<BAT>
-If the character used the Bat, the clues would be
-(Fingerprint from the bat, the stolen record of the bat from a baseball team, and Steve Tibb's email or voice mail ) 
-
-Finding the bat
-1. Find a broken bat from somewhere
-2. Find the fingerprint from the bat and use it as a clue
-3. Narrowed down to few people
-
-Finding the stolen record from the baseball team
-4. Find a stolen record from baseball teams
-5. Interviewed and found out that thy had a visitor
-6. Narrowed down to fewer people
-
-Listen to steve tibb's email or voice mail
-7. Listen to steve tibb's email or voice and figure out why someone hated Steve Tibbs
-
-
-<INJECTION>
-If the character used the Injection, the clues would be
-(Finding what medicine injured steve tibbs, Find Hospital CCTV and record of the stolen medicine,  and Steve Tibb's email or voice mail ) 
-
-Finding which medicine killed steve tibbs
-1. Go to the hospital Steve Tibbs 
-2. Ask the nurse which medicine was injected and learned about the medicine, but find out that he is misssing. 
-3. Got enough detox medicine in case steve tibbs might need it. 
-4. Also, go to the outreach office and got the voluntary outreach member list, and narrowed down the suspect.
-
-Finding hospital CCTV to see who stole the medicine
-4. Go to the security office   ask for a CCTV record. 
-5. Find out a suspisious entrance to the hospital. 
-6. See found that person resembles the narrwoed down list.
-
-Finding Steve Tibb's email or voice mail.  
-7. Found out that steve tibbs was gloomy that he was trying to commit suicide and asked someone to do it.  
-
-
-<ROPE> 
-If the character used the Rope, the clues would be
-( CCTV record of Ferst, tracking back the personal belongings in the scene, the purchase record of the rope ,  and Steve tIbb's emial or voice mail)
-You hear Steve Tibb's scream, and run to that location. (in the back of Ferst)
-
-Tracking back the personal belongings of the scene 
-1. You see a rope hanging, and steve tibb's (some) personal belongings left, and maybe some other person's belongings, but Steve tibbs and the suspect disappeared. 
-2. You take a picutre of the beloningings, and pack them.
-3. You go meet with Steve Tibb's brother to ask which one belongs to Tibbs and which one don't
-4. You found out that some items doesn't belong to Tibbs. - These items are a pen, a note, and a pocketknife
-5. You see the note and found out he is an engineer (?), and find a company name written in a pen: GNE , which you narrowed down to suspects
-6. 
-
-FInding CCTV record of the Ferst
-1. Ask the security guard for the record
-2. See someone acting as a plumber, and getting in to the back room. 
-3. 
-
-See Steve Tibb's email or voice mail 
-
-<CAR> 
-If the character used the CAR , the clues would be
-(The sound of screaming-witness, the glove left in the car, and the school bus record, Steve tibb's email or voice mail. )
-
-Find the sound source, and meet with witnesses
-1. You meet with the witnesses and ask what they saw. But they didn't see much, yet saw the driver running away with Tibb's body. 
-2. There was no children in the bus, you learned that the accident happened at 12 pm. 
-3. You learned that the the schoolbus is from the school visit to Georgia tech, which belongs to the nearby public school. 
-
-Find the left belongings
-1. You inspect the location and found that the driver left his glove and hat in the scene. 
-2. You take those belongings and found a piece of hair. You do a DNA test to find out who it is. 
-
-Find the school bus record. (witness 2) 
-1. You ask the original bus driver and ask how he left is key in the bus. 
-2. You figure out that the bus driver was being spied and was stolen the bus when he left to smoke and take a break. 
-
-Hack Steve Tibb's email.
-You hack Steve Tibb's email and figured out that the person was just envy of Tibb's fame and wanted to take over the company. 
-
 
 
 gun,
@@ -168,9 +11,112 @@ car
   
 
 */
+//class of tools used for killing
+(function (window) {
+  function tool(name, evidence   ) {
+        this.name     = name;
+        this.evidence=evidence;
+        
+        this.questAccomplished = new Array();
+        this.questTried = new Array();
 
-function quest() {
+       
+        }
+        
+        
+        
+ function inspecttools(name, clue) {
+        this.name     = name;
+       
+  }
+        
+  }(window));   //but i dont understand what this window stuff is doing. do i need it to make a class? 
+       
+        
+        
+        ////
 
-    
-    
+function initquest(tool, killer, crimescene) {
+this.tool=tool;
+this.killer=killer;
+this.crimescene=crimescene;
+
+this.knowtool=false;
+this.knowkiller=false;
+this.knowlocation=false;    
+this.knowwhy=false;    
+
+ 
+
+
+itemrequirements(); 
 }
+
+  function itemrequirements(){
+
+  switch(this.tool) {
+  case "gun":
+  fingerprint;
+  
+  break;
+  
+  
+  case "poison":
+  
+  break;
+  
+  
+    case "bat":
+  
+  break;
+  
+  
+    case "injection":
+  
+  break;
+  
+  
+    case "rope":
+  
+  break;
+  }
+  
+  
+
+
+function givequest(step){
+ 
+
+  switch(step) {
+  case "finditem":
+  
+  
+  break;
+  
+  
+  case "inspectitem":
+  
+  break;
+  
+  
+ case "meetdetective":
+  
+  break;
+  
+  
+  case "inspectcollaborator":
+  
+  break;
+  
+  
+ case "meetfamily":
+  
+  break;
+  
+  
+ case "findmessage":
+  
+  break;
+  
+  }
+  
