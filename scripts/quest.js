af7b1b0b6df38fb35a1a279d310d3635016116c4
Lianghao <chenlh@gatech.edu>
2014-12-05 10:48:23 -0500

    update

--- scripts/quest.js
+++ scripts/quest.js
@@ -117,65 +117,65 @@ function questGenerator(tool, killer, crimescene) {
     switch (this.tool){
     	case "gun":
     	
-    	this.allQuests.push(new quest("object","find the gun",ll.ferst, 'no', 'got gun as an evidence'));
-    	this.allQuests.push(new quest("object-investigate","investigate the fingerprints on the gun",ll.clough, 'yes', 'got fingerprints as evidence'));
+    	this.allQuests.push(new quest("object","find the gun",ll.ferst, 'none', 'got gun as an evidence'));
+    	this.allQuests.push(new quest("object-investigate","investigate the fingerprints on the gun",ll.clough, 'yes', "Evidence: Fingerprints on the gun"));
     	
-		this.allQuests.push(new quest("record","meet with detectives",ll.fountain, 'no', 'meet detectives to get information about the gun dealers'));
+		this.allQuests.push(new quest("record","meet with detectives",ll.fountain, 'none', "Evidence: Information about gun dealer"));
     	this.allQuests.push(new quest("record-investigate","investigate gun dealer records",ll.skiles, 'yes', 'got gun dealer record as an evidence'));
     	
-    	this.allQuests.push(new quest("personal","find the password",ll.library, 'no', 'meet with Tibbs family to ask for his email/password'));
-    	this.allQuests.push(new quest("personal-investigate","Go to computerlab to investigate email",ll.vanLeer, 'no', 'Got Threat email sent to Tibbs'));
+    	this.allQuests.push(new quest("personal","find the password",ll.library, 'none', 'meet with Tibbs family to ask for his email/password'));
+    	this.allQuests.push(new quest("personal-investigate","Go to computerlab to investigate email",ll.vanLeer, 'none', "Evidence: Tibb has received a threatening email. "));
     	
     	
     		break;
     	case "poison":
     	
-    	this.allQuests.push(new quest("object","find the wineglass",ll.ferst, 'no', 'got a wineglass as an evidence'));
-    	this.allQuests.push(new quest("object-investigate","investigate the fingerprints on the wineglass",ll.clough, 'yes', 'got fingerprints as evidence'));
+    	this.allQuests.push(new quest("object","find the wineglass",ll.ferst, 'none', "Evidence: broken wine glass"));
+    	this.allQuests.push(new quest("object-investigate","investigate the fingerprints on the wineglass",ll.clough, 'yes', "Evidence: Wine glass has fingerprints. "));
     	
-		this.allQuests.push(new quest("record","meet with police",ll.fountain, 'no', 'got note and book lists as an evidence'));
+		this.allQuests.push(new quest("record","meet with police",ll.fountain, 'none', "Evidence: Note about the poison shows that the suspect borrowed books from the library. "));
     	this.allQuests.push(new quest("record-investigate","investigate the notes",ll.library, 'yes', 'got library check out record an evidence'));
     	
-    	this.allQuests.push(new quest("personal","find the diary",ll.studentCenter, 'no', 'meet with Tibbs family to ask for his diary'));
-    	this.allQuests.push(new quest("personal-investigate","Go to Tibbs home to pick up and investigate the diary",ll.library, 'no', 'Diary writes about the suspect and Tibb romantic triangle'));
+    	this.allQuests.push(new quest("personal","find the diary",ll.studentCenter, 'none', "Evidence: Family knows the location of Tibb's diary"));
+    	this.allQuests.push(new quest("personal-investigate","Go to Tibbs home to pick up and investigate the diary",ll.library, 'none', "Evidence:  Tibb's diary shows that he had a heart breaking experience with friends in a romantic triangle. "));
  
     	
     		break;
     	case "bat":
     	
-    	this.allQuests.push(new quest("object","find the bat",ll.ferst, 'no', 'got a broken bat'));
+    	this.allQuests.push(new quest("object","find the bat",ll.ferst, 'none', 'got a broken bat'));
     	this.allQuests.push(new quest("object-investigate","investigate the DNA of hair on the bat",ll.clough, 'yes', 'got DNA as evidence'));
     	
-		this.allQuests.push(new quest("record","meet with captain",ll.studentCenter, 'no', 'got stolen record of the bat'));
+		this.allQuests.push(new quest("record","meet with captain",ll.studentCenter, 'none', 'got stolen record of the bat'));
     	this.allQuests.push(new quest("record-investigate","investigate the cctv",ll.fountain, 'yes', 'got cctv record of the suspect stealing a bat'));
     	
-    	this.allQuests.push(new quest("personal","find the enemy",ll.library, 'no', 'meet with Tibbs officemates to know tibb enemy who is a psychopass'));
-    	this.allQuests.push(new quest("personal-investigate","Go to Tibbs desk to find any data ",ll.vanLeer, 'no', 'Suspect left a threatening letter on Tibb desk  '));
+    	this.allQuests.push(new quest("personal","find the enemy",ll.library, 'none', 'meet with Tibbs officemates to know tibb enemy who is a psychopass'));
+    	this.allQuests.push(new quest("personal-investigate","Go to Tibbs desk to find any data ",ll.vanLeer, 'none', 'Suspect left a threatening letter on Tibb desk  '));
  		 break;
   
   
     	case "injection":
-    	this.allQuests.push(new quest("object","find the syringe",ll.ferst, 'no', 'got a syringe and clothes'));
+    	this.allQuests.push(new quest("object","find the syringe",ll.ferst, 'none', 'got a syringe and clothes'));
     	this.allQuests.push(new quest("object-investigate","investigate the DNA of hair on the clothes",ll.fountain, 'yes', 'got DNA as evidence'));
     	
-		this.allQuests.push(new quest("record","meet with nurses",ll.healthCenter, 'no', 'got suspicious hospital record'));
+		this.allQuests.push(new quest("record","meet with nurses",ll.healthCenter, 'none', 'got suspicious hospital record'));
     	this.allQuests.push(new quest("record-investigate","investigate the cctv",ll.fountain, 'yes', 'got cctv record of the suspect with suspicious activity'));
     	
-    	this.allQuests.push(new quest("personal","find tibbs secret",ll.library, 'no', 'meet with Tibbs family to find out his secret about drug usage '));
-    	this.allQuests.push(new quest("personal-investigate","Listen to Tibbs voicemail ",ll.vanLeer, 'no', 'Suspect left a threatening voice mail  '));
+    	this.allQuests.push(new quest("personal","find tibbs secret",ll.library, 'none', 'meet with Tibbs family to find out his secret about drug usage '));
+    	this.allQuests.push(new quest("personal-investigate","Listen to Tibbs voicemail ",ll.vanLeer, 'none', 'Suspect left a threatening voice mail  '));
 
         break;
     		
     		
     	case "rope":
-    	this.allQuests.push(new quest("object","find the rope",ll.ferst, 'no', 'found rope and personal belongings'));
+    	this.allQuests.push(new quest("object","find the rope",ll.ferst, 'none', 'found rope and personal belongings'));
     	this.allQuests.push(new quest("object-investigate","investigate the DNA of hair from the belongings",ll.clough, 'yes', 'got DNA as evidence'));
     	
-		this.allQuests.push(new quest("record","meet with security",ll.studentCenter, 'no', 'got witnesses about mailman visits'));
+		this.allQuests.push(new quest("record","meet with security",ll.studentCenter, 'none', 'got witnesses about mailman visits'));
     	this.allQuests.push(new quest("record-investigate","investigate the cctv",ll.fountain, 'yes', 'got cctv record of the bldg and a fake mailman'));
     	
-    	this.allQuests.push(new quest("personal","find the enemy",ll.library, 'no', 'meet with Tibbs friends to know tibbs fan who is a psychopass'));
-    	this.allQuests.push(new quest("personal-investigate","Go to Tibbs desk to find data from phone ",ll.vanLeer, 'no', 'Suspect left a threatening text message '));
+    	this.allQuests.push(new quest("personal","find the enemy",ll.library, 'none', 'meet with Tibbs friends to know tibbs fan who is a psychopass'));
+    	this.allQuests.push(new quest("personal-investigate","Go to Tibbs desk to find data from phone ",ll.vanLeer, 'none', 'Suspect left a threatening text message '));
     	
     	break;
     	
