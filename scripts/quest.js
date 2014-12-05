f8146e8f9aaa7d2d9b854629946210e5e7bef089
jeanhochu <jeanhochu@gmail.com>
2014-12-05 14:05:05 -0500

    Update quest.js

--- scripts/quest.js
+++ scripts/quest.js
@@ -24,31 +24,31 @@ function questGenerator(tool, killer, crimescene) {
     switch (this.tool){
     	case "gun":
     		this.allEvidences.push("Evidence: Bullet is left in the scene");
-    		this.allEvidences.push("Evidence: Tibb had a grim when he mentioned in his talk to thank his family. ");
+    		this.allEvidences.push("Evidence: Tibb had a grim when he mentioned in his talk to thank his family");
     		this.allEvidences.push("Evidence: Gun dealer is nearby. ");
     		
     		
     		this.allEvidences.push("Evidence: Fingerprints on the gun");
     		this.allEvidences.push("Evidence: Information about gun dealer");
-    		this.allEvidences.push("Evidence: Tibb's password and email");
-    		this.allEvidences.push("Evidence: Tibb has received a threatening email. ");
+    		this.allEvidences.push("Evidence: Tibbs password and email");
+    		this.allEvidences.push("Evidence: Tibb has received a threatening email");
     	    imageURL="<center><img src='img/item-gun.jpg' id='item'></img></center>";
-    	    deathInfo="Tibb seems to be shot by a gun but his body disappeared. Many people heard the trigger, bullet is found, Gun dealer is nearby, Tibb mentioned his family with a grim during his talk. "; 
+    	    deathInfo="Tibb seems to be shot by a gun but his body disappeared. Many people heard the trigger, bullet is found, Gun dealer is nearby, Tibb mentioned his family with a grim during his talk."; 
     	
     	
     		break;
     	case "poison":
     		this.allEvidences.push("Evidence: broken wine glass");
     		this.allEvidences.push("Evidence: the note about the poison");
-    	    this.allEvidences.push("Evidence: Tibb mentioned in his talk to thank his friends with a grim. ");
+    	    this.allEvidences.push("Evidence: Tibb mentioned in his talk to thank his friends with a grim");
     				
-    		this.allEvidences.push("Evidence: Wine glass has fingerprints. ");
-   			 this.allEvidences.push("Evidence: Note about the poison shows that the suspect borrowed books from the library. ");
-    		this.allEvidences.push("Evidence: Family knows the location of Tibb's diary");
-    		this.allEvidences.push("Evidence:  Tibb's diary shows that he had a heart breaking experience with friends in a romantic triangle. ");
+    		this.allEvidences.push("Evidence: Wine glass has fingerprints");
+   			 this.allEvidences.push("Evidence: Note about the poison shows that the suspect borrowed books from the library");
+    		this.allEvidences.push("Evidence: Family knows the location of Tibbs diary");
+    		this.allEvidences.push("Evidence:  Tibbs diary shows that he had a heart breaking experience with friends in a romantic triangle");
     		
     	    imageURL="<center><img src='img/items-poison.jpg' id='item'></img></center>";
-    	    deathInfo="Tibb drank wine and got fainted dropping the wine glass. There is a note on the scene but police took it. Witnesses are saying that Tibb had a secret which might be written in his diary. "; 
+    	    deathInfo="Tibb drank wine and got fainted dropping the wine glass. There is a note on the scene but police took it. Witnesses are saying that Tibb had a secret which might be written in his diary"; 
     	
     		
     		break;
@@ -68,12 +68,12 @@ function questGenerator(tool, killer, crimescene) {
     	case "injection":
     	   this.allEvidences.push("Evidence: Syringe and clothes of the nurse left in the scene");
     		this.allEvidences.push("Evidence: Package of the syringe with the name of the hospital");
-    	    this.allEvidences.push("Evidence: Tibb mentioned in his talk he has a secret for his happiness. ");
+    	    this.allEvidences.push("Evidence: Tibb mentioned in his talk he has a secret for his happiness");
     				
-    		this.allEvidences.push("Evidence: DNA test of left hair from the clothes in the scene ");
-   			 this.allEvidences.push("Evidence: Hospital record with suspicious activities. ");
-    		this.allEvidences.push("Evidence: CCTV of the hospital ");
-    		this.allEvidences.push("Evidence: Tibb's family's witnesses about Tibb's drug usage. ");
+    		this.allEvidences.push("Evidence: DNA test of left hair from the clothes in the scene");
+   			 this.allEvidences.push("Evidence: Hospital record with suspicious activities");
+    		this.allEvidences.push("Evidence: CCTV of the hospital");
+    		this.allEvidences.push("Evidence: Tibb's family's witnesses about Tibb's drug usage");
     		this.allEvidences.push("Evidence:  Voicemail sent to Tibbs from  a drug dealer /nurse");
             imageURL="<center><img src='img/items-injection.png' id='item'></img></center>";
     	    deathInfo="Tibbs seems to be injected but his body disappeared and there is a syringe. He mentioned in his talk that he has a secret for his happiness that his family knows. "; 
@@ -82,12 +82,13 @@ function questGenerator(tool, killer, crimescene) {
     	case "rope":
     	 this.allEvidences.push("Evidence: Scream from the scene");
     		this.allEvidences.push("Evidence: Security said only mailman has access to the back of the bldg");
-    	    this.allEvidences.push("Evidence: Tibb paused during his talk for a few seconds, being scared to see an unexpected audience. ");
+    	    this.allEvidences.push("Evidence: Tibb paused during his talk for a few seconds, being scared to see an unexpected audience");
     				
-    		this.allEvidences.push("Evidence: DNA test of personal belongings left in the scene ");
-   			this.allEvidences.push("Evidence: CCTV of the bldg ");
-    		this.allEvidences.push("Evidence: Tibb's friends witnesses that he had a  fans who was a psychopass and hated him. ");
-    		this.allEvidences.push("Evidence: Threatening textmessage sent to Tibbs ");
+    		this.allEvidences.push("Evidence: DNA test of personal belongings left in the scene");
+   			this.allEvidences.push("Evidence: CCTV of the bldg");
+    		this.allEvidences.push("Evidence: Tibb's friends witnesses that he had a  fans who was a psychopass and hated him");
+    		this.allEvidences.push("Evidence: Threatening textmessage sent to Tibbs");
+         this.allEvidences.push("Evidence: found rope and personal belongings");
          
             imageURL="<center><img src='img/items-injection.png' id='item'></img></center>";
     	    deathInfo="You heard a scream. Tibb seems to be choked by a rope but his body disappeared, and only the rope is left. Tibb paused for his talk for a few seconds, being scared to see an unexpected audience. Security says that only mailman has access to the back of the bldg.  "; 
@@ -168,14 +169,14 @@ function questGenerator(tool, killer, crimescene) {
     		
     		
     	case "rope":
-    	this.allQuests.push(new quest("object","find the rope",ll.ferst, 'none', 'found rope and personal belongings'));
-    	this.allQuests.push(new quest("object-investigate","investigate the DNA of hair from the belongings",ll.clough, 'yes', 'got DNA as evidence'));
+    	this.allQuests.push(new quest("object","find the rope",ll.ferst, 'none', 'Evidence: found rope and personal belongings'));
+    	this.allQuests.push(new quest("object-investigate","investigate the DNA of hair from the belongings",ll.clough, 'yes', 'Evidence: DNA test of personal belongings left in the scene'));
     	
-		this.allQuests.push(new quest("record","meet with security",ll.studentCenter, 'none', 'got witnesses about mailman visits'));
-    	this.allQuests.push(new quest("record-investigate","investigate the cctv",ll.fountain, 'yes', 'got cctv record of the bldg and a fake mailman'));
+		this.allQuests.push(new quest("record","meet with security",ll.studentCenter, 'none', 'Evidence: Security said only mailman has access to the back of the bldg'));
+    	this.allQuests.push(new quest("record-investigate","investigate the cctv",ll.fountain, 'yes', 'Evidence: CCTV of the bldg '));
     	
-    	this.allQuests.push(new quest("personal","find the enemy",ll.library, 'none', 'meet with Tibbs friends to know tibbs fan who is a psychopass'));
-    	this.allQuests.push(new quest("personal-investigate","Go to Tibbs desk to find data from phone ",ll.vanLeer, 'none', 'Suspect left a threatening text message '));
+    	this.allQuests.push(new quest("personal","find the enemy",ll.library, 'none', 'Evidence: Tibbs friends witnesses that he had a  fans who was a psychopass and hated him'));
+    	this.allQuests.push(new quest("personal-investigate","Go to Tibbs desk to find data from phone ",ll.vanLeer, 'none', 'Evidence: Threatening textmessage sent to Tibbs '));
     	
     	break;
     	
